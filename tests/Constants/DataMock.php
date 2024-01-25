3a63f51a806f5d3dc0c928c3d398ec5d2e89be3d
nguyenhy <nguyenhy311@gmail.com>
2024-01-25 16:48:02 +0700

    test: add `DataMock`

---
+++ tests/Constants/DataMock.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AgoraRoomServiceTests\Constants;
+
+class DataMock
+{
+    const Account        = "^ZSgT<%q:Fj*@`92>#OHL?\"hkm~nGYiP";
+    const AppCertificate = "5CFd2fd1755d40ecb72977518be15d3b";
+    const AppId          = "970CA35de60c44645bbae8a215061b33";
+    const ChannelName    = "7d72365eb983485397e3e3f9d460bdda";
+    const Expire         = 600;
+    const Uid            = 2882341273;
+    const UidStr         = "2882341273";
+    const IssueTs        = 1706165540;
+    const Salt           = 1;
+}
